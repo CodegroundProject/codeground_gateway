936073355213a045c5dcb5142c3b56caf8c6432e
MohamedGouaouri <im_gouaouri@esi.dz>
2022-05-27 09:45:05 +0100

    connect zuul to auth service

---
+++ src/main/java/com/esi/codeground/configurations/GatewayConfiguration.java
@@ -0,0 +1,19 @@
+package com.esi.codeground.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/auth/**")
+                .permitAll()
+                .antMatchers("/**")
+                .authenticated();
+    }
+}
